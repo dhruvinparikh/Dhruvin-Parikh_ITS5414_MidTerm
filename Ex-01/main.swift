fac75328e9fd75a3b57c86b881cd7be10c28ff02
Parikh Dhruvin S <n01150847@nl-00115-013.local>
2018-03-21 08:32:52 -0400

    Initial Commit

---
+++ Ex-01/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Ex-01
+//
+//  Created by Parikh Dhruvin S. on 3/21/18.
+//  Copyright © 2018 Parikh Dhruvin S. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
